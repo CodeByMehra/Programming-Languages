29f17e0b54a25a92d03505d49267b026c7705fae
Vishal <149322538+CodeByMehra@users.noreply.github.com>
2025-07-07 19:01:47 +0530

    Update 05_Conditionals.py

--- Python/05_Conditionals.py
+++ Python/05_Conditionals.py
@@ -14,7 +14,7 @@
 age = 18
 
 if age >= 18:
-    print("You are eligible for voting.")
+    print("You are eligible to vote.")
 
 # -------------------------------------------
 # ✅ if-else Statement
